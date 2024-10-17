9ecc3a88bb9a8e90761eb7a14d5b1a04857b352f
coderPashka <pavel.khorolsky5000@gmail.com>
2024-10-17 15:29:18 +0300

    Binary_Insertion_Sort.h

--- Семинар 2/sorting/sorting/Binary_Insertion_Sort.h
+++ Семинар 2/sorting/sorting/Binary_Insertion_Sort.h
@@ -1,3 +1,4 @@
 #pragma once
 #include "Binary_Insertion_Sort.cpp"
+template <typename T> int BinarySearch(int arr[], int item, int low, int high);
 template <typename T> void BinaryInsertionSort(T* arr, int n);
